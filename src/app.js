ab1ddafdaaafebc6469c3b85b854313409ba08ce
Tyler <ty.jbailey32@gmail.com>
2021-01-27 16:27:05 -0800

    Server renders html

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 
 // Create server
 const server = http.createServer(function(req, res) {
-    res.write('Response write');
-    res.end();
+    // Tell server to render html
+    res.writeHead(200, {'Content-Type': 'text/html'}); // Status code 200 = success
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404); // Status code 404 = error since can't find html we are trying to write
+            res.write('Error: File not found');
+        } else {
+            res.write(data);
+        }
+        res.end();
+    })
 })
 
 // Attempt to run server - listen on specified port for requests
