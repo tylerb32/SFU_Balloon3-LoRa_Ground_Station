58f1db7a4e63d55482d49500ef5bd69f22c47a0a
Tyler <ty.jbailey32@gmail.com>
2021-02-21 23:20:26 -0800

    Added launch zone idea placeholder.

--- src/ground_station.js
+++ src/ground_station.js
@@ -1,6 +1,6 @@
 
 // Leaflet Map Creation
-var map = L.map('map').setView([51.483667, -113.142667], 13); // Launch Site
+let map = L.map('map').setView([51.483667, -113.142667], 13); // Launch Site
 //var map = L.map('map').setView([49.277693,-122.9209615], 11); // SFU Burnaby
 L.tileLayer('/tiles/{z}/{x}/{y}.png', {
     // Setup map attributes
@@ -10,9 +10,19 @@ L.tileLayer('/tiles/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add launch sute marker
-var launchSiteMarker = L.marker([51.483667, -113.142667]).addTo(map);
+let launchSiteMarker = L.marker([51.483667, -113.142667]).addTo(map);
 //var launchSiteMarker = L.marker([49.277693,-122.9209615]).addTo(map);
 launchSiteMarker.bindPopup("<b>YO?</b>").openPopup();
+
+// IDEA
+// Based on GPS data project the landing zone
+let landingZone = L.circle([51.583667, -113.242667], {
+    color: 'red',
+    fillColor: 'red',
+    fillOpacity: 0.5,
+    radius: 5000
+}).addTo(map);
+landingZone.bindTooltip("Projected Landing Zone").openTooltip();
 
 // Old code for using mbtiles library for leaflet (not needed unless switching tileset)
 //    L.tileLayer.mbTiles('Addon-NA11-J60.mbtiles', {
