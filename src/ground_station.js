0c71a1d9114c3d781240d6934b491a75c69d04e4
Tyler <ty.jbailey32@gmail.com>
2021-02-22 00:45:36 -0800

    Started settings management

--- src/ground_station.js
+++ src/ground_station.js
@@ -1,8 +1,7 @@
-
 // Leaflet Map Creation
 let map = L.map('map').setView([51.483667, -113.142667], 13); // Launch Site
 //var map = L.map('map').setView([49.277693,-122.9209615], 11); // SFU Burnaby
-L.tileLayer('/tiles/{z}/{x}/{y}.png', {
+L.tileLayer(tileDir + '/{z}/{x}/{y}.png', {
     // Setup map attributes
     minZoom: 13,
     maxZoom: 13
@@ -14,6 +13,13 @@ let launchSiteMarker = L.marker([51.483667, -113.142667]).addTo(map);
 //var launchSiteMarker = L.marker([49.277693,-122.9209615]).addTo(map);
 launchSiteMarker.bindPopup("<b>YO?</b>").openPopup();
 
+//let marker1 = createMarker("Marker 1", [51.483667, -113.142667]);
+//let marker2 = createMarker("Marker 2", [51.383667, -113.042667]);
+// L.polyline([[51.483667, -113.142667], [51.383667, -113.042667]], {
+//     color: 'blue',
+//     smoothFactor: 2.0
+// }).addTo(map);
+
 // IDEA
 // Based on GPS data project the landing zone
 let landingZone = L.circle([51.583667, -113.242667], {
@@ -24,8 +30,23 @@ let landingZone = L.circle([51.583667, -113.242667], {
 }).addTo(map);
 landingZone.bindTooltip("Projected Landing Zone").openTooltip();
 
-// Old code for using mbtiles library for leaflet (not needed unless switching tileset)
-//    L.tileLayer.mbTiles('Addon-NA11-J60.mbtiles', {
-//        minZoom: 11,
-//        maxZoom: 15
-//    }).addTo(map); // TODO: Try using mbutils to export pbf instead of png
+// Create a marker containing location data and google maps directions link. Adds marker to map and returns the marker object
+// TODO: Add parameter for colour
+// TODO: Add time information
+// TODO: Make google maps hyperlink open in new tab
+function createMarker(title, location) {
+    let getRoute = "Get Route";
+    // How to format the maps query: https://developers.google.com/maps/documentation/urls/get-started
+    let mapsQuery = "https://google.ca/maps/dir/?api=1&destination=" + location[0] + "," + location[1]
+    let mapsLink = getRoute.link(mapsQuery);
+    let marker = L.marker(location).addTo(map);
+    marker.bindPopup("<b>"+ title + "</b><br>" + location[0] + ", " + location[1] + "</br><b>" + mapsLink + "</b>").openPopup();
+    return marker;
+}
+
+// Get the string value of the specified setting
+async function getSetting(setting) {
+    let response = await fetch("/src/settings.json");
+    let parsedObj = await response.json();
+    return parsedObj[setting];
+}
