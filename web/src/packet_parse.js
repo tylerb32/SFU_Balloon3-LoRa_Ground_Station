a65140cd63e09ba9ba5fc814a88af45dba86f208
tylerb32 <ty.jbailey32@gmail.com>
2021-08-15 14:51:51 -0700

    Switched from tail to using fs watch.

--- web/src/packet_parse.js
+++ web/src/packet_parse.js
@@ -1,8 +1,35 @@
-const Tail = require('tail').Tail;
+const fs = require('fs');
+const eol = require('os').EOL;
 
-const dataPath = './data/data.txt';
-let tail = new Tail(dataPath);
-tail.watch();
-tail.on("line", data => {
-    console.log(data);
+const filePath = '../data/data.txt';
+let fileSize = fs.statSync(filePath).size;
+// fs.watch(filePath, function(event, trigger) {
+fs.watchFile(filePath, function (current, previous) {
+    //if (trigger) {
+    if (current.mtime <= previous.mtime) {
+        return;
+    }
+    
+    let newFileSize = fs.statSync(filePath).size;
+    let sizeDiff = newFileSize - fileSize;
+
+    if (sizeDiff < 0) {
+        fileSize = 0;
+        sizeDiff = newFileSize;
+    }
+
+    let buffer = new Buffer.alloc(sizeDiff);
+    let fileDes = fs.openSync(filePath, 'r');
+    fs.readSync(fileDes, buffer, 0, sizeDiff, fileSize);
+    fs.closeSync(fileDes);
+
+    fileSize = newFileSize;
+    parseBuffer(buffer);
+    //}
 });
+
+function parseBuffer(buffer) {
+    buffer.toString().split(eol).forEach(function (line) {
+        console.log(line);
+    });
+}
